d679b1f6d7e27e648eb5561922a86259fddcca16
vmcuong <vmcuong33@gmail.com>
2017-11-20 08:36:52 +0700

    da

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
-
+var fs = require("fs");
+var ObjectId = require('mongodb').ObjectId;
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
